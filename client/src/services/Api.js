9e92ea0db96b4f444e12482f14f92f76031e40a8
aaron-schuyler <aaron@yeahthatcontent.com>
2020-02-17 14:33:55 -0500

    update 3.0

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,6 +3,6 @@ axios.defaults.withCredentials = true; // enable axios post cookie, default fals
 
 export default () => {
     return axios.create({
-        baseURL: `http://18.218.24.115:8000`
+        baseURL: `http://ec2-18-216-99-42.us-east-2.compute.amazonaws.com:8000`
     });
 };
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 //cors
 const cors = require('cors')
 const corsOptions = {
-    origin: ['97.82.169.245:8080', '97.82.169.245:3000'],
+    origin: ['http://97.82.169.245:8080', 'http://97.82.169.245:3000'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], //the port my react app is running on.
     credentials: true,
 }
